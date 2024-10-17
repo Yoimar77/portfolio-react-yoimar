2c9b9ca5df0123007277f5f513d8a03f8debcf16
samuraypro7767 <sebaspro7767@gmail.com>
2024-10-17 18:20:35 -0500

    Ajustes básicos implementados

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Header } from '../../Layouts/Header/Header'
+
 
 export const Home = () => {
   return (
+    <>
+    <Header/>
     <div>Home</div>
+    </>
+    
   )
 }
